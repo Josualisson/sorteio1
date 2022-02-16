5685cb619c596d8b37fcf57d09cf3efde042b55e
Josué Alisson <josualisson17@gmail.com>
2022-02-16 00:06:50 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function sorteio(){
+    let sort = Math.floor(Math.random() * 10 )
+
+    document.getElementById("resultado").innerHTML=sort;
+}
+
+function sorteio1(){
+    let sort = Math.floor(Math.random() * 10 )
+
+    document.getElementById("resultado1").innerHTML=sort;
+}
+
+function sorteio2(){
+    let sort = Math.floor(Math.random() * 10 )
+
+    document.getElementById("resultado2").innerHTML=sort;
+}
+function sorteio3(){
+    let sort = Math.floor(Math.random() * 10 )
+
+    document.getElementById("resultado3").innerHTML=sort;
+}
+function sorteio4(){
+    let sort = Math.floor(Math.random() * 10 )
+
+    document.getElementById("resultado4").innerHTML=sort;
+}
